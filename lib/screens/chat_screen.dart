ba0d16d3e19f19e095eb7c6e5dc7de91555946d6
Madiha Mallick <70858557+madihamallick@users.noreply.github.com>
2021-07-21 21:23:04 +0530

    Added Chat Screen
    Added Chat Screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:chitchat/utils/constants.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -29,11 +30,71 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(
-        //TODO Implement Chat Screen
-        child: Text('Chat screen'),
-      ),
-    );
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+    return Scaffold(
+        backgroundColor: ChatScreenBackground,
+        appBar: AppBar(
+          backgroundColor: ChatScreenAppBar,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(5.0, 0.0, 10.0, 5.0),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Container(
+                            child: TextField(
+                              autofocus: false,
+                              style: new TextStyle(
+                                  fontSize: 23.0, color: Color(0xFFbdc6cf)),
+                              decoration: new InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                hintText: 'Type here...',
+                                contentPadding: const EdgeInsets.only(
+                                    left: 14.0, bottom: 15.0, top: 15.0),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      new BorderSide(color: Colors.white),
+                                  borderRadius: new BorderRadius.circular(25.7),
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      new BorderSide(color: Colors.white),
+                                  borderRadius: new BorderRadius.circular(25.7),
+                                ),
+                              ),
+                              maxLines: 1,
+                              onChanged: (value) {},
+                            ),
+                            width: queryData.size.width * 0.75,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(CircleBorder()),
+                            padding:
+                                MaterialStateProperty.all(EdgeInsets.all(30)),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ]))
+          ],
+        ));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,9 @@ const Color kAppBarColor = Color(0xFFFFFFFF);
 const Color kAppBarIconColor = Colors.black54;
 const Color kRegisterButtonColor = Color(0xffE8CC09);
 const Color kLoginButtonColor = Color(0xff5143A5);
+const Color ChatScreenAppBar = const Color(0xff315659);
+const Color ChatScreenBackground = const Color(0xffDFDFDF);
+const Color ChatButtonBorder = const Color(0xff9E8A8A);
 
 //Images
 const AssetImage kLogo = AssetImage('images/logo.png');
